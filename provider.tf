9ff07cb60a4bbc0ad192f8fb52b9c2069dc0bf0a
Isac Cavalcante <isac.cavalcante@hp.com>
2019-04-02 14:50:49 -0300

    Create terraform subfolder

--- provider.tf
+++
@@ -1,6 +0,0 @@
-// Configure the Google Cloud provider
-provider "google" {
-    credentials = "${file("terraform-account.json")}"
-    project     = "my-project-012345" // change here
-    region      = "us-west1"
-}
